69da6892e0531353ff20491b03e79b22938c71b4
jhonnyperalta <jhonnyperaltasis@gmail.com>
2018-05-22 18:56:35 -0500

    cards

--- src/views/Catalogo.js
+++ src/views/Catalogo.js
@@ -69,6 +69,31 @@ class Catalogo extends Component {
 
           </div>
         </nav>
+           
+          <div className=" card horizontal">
+            <div className="card-image">
+        <img src="https://lorempixel.com/100/190/nature/6"/>
+            </div>
+
+            <div class="card-stacked">
+          <div className="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.</p>
+        </div>
+        <div className="card-tabs">
+      <ul className="tabs tabs-fixed-width">
+        <li className="tab"><a href="#test4">Test 1</a></li>
+        <li className="tab"><a class="active" href="#test5">Test 2</a></li>
+        <li className="tab"><a href="#test6">Test 3</a></li>
+      </ul>
+    </div>
+    <div className="card-content grey lighten-4">
+      <div id="test4">Test 1</div>
+      <div id="test5">Test 2</div>
+      <div id="test6">Test 3</div>
+    </div>
+            </div>
+           
+          </div>
       </div>
     );
   }
